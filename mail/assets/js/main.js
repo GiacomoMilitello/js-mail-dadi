49d226106f87ec2ac72f88f1348d4b97d990c56d
GiacomoMilitello <g.militello95@gmail.com>
2024-01-11 15:58:45 +0100

    email js "for" not "includes"

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -1,3 +1,4 @@
+/*
 // Email autorizzate
 const emailAutorizzate = ["alessio&hyur@gmail.com", "alessandro&elisabetta@libero.it"];
 
@@ -9,6 +10,31 @@ buttonOk.addEventListener("click", function() {
     containerRisultato.classList.remove("d-none");
 
     if (emailAutorizzate.includes(emailInput)) {
+        document.getElementById("risultato").innerHTML = "Sei autorizzato";
+    } else {
+        document.getElementById("risultato").innerHTML = "Non sei autorizzato";
+    }
+});
+*/
+
+// Email autorizzate
+const emailAutorizzate = ["alessio&hyur@gmail.com", "alessandro&elisabetta@libero.it"];
+
+const buttonOk = document.getElementById("ok");
+
+buttonOk.addEventListener("click", function() {
+    const emailInput = document.getElementById("email").value;
+    var containerRisultato = document.getElementById("container-risultato");
+    containerRisultato.classList.remove("d-none");
+
+    var autorizzato = false;
+    for (let i = 0; i < emailAutorizzate.length; i++) {
+        if (emailAutorizzate[i] === emailInput) {
+            autorizzato = true;
+        }
+    }
+
+    if (autorizzato === true) {
         document.getElementById("risultato").innerHTML = "Sei autorizzato";
     } else {
         document.getElementById("risultato").innerHTML = "Non sei autorizzato";
